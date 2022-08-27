bfaef9ec2b897dcc513e94079e4caf6bdbd1dfcb
swapped004 <swapnildey007@gmail.com>
2022-08-27 22:03:22 +0600

    merged with prv commit and made some changes to icons for dependency reasons

--- src/components/Exercise/fileItem.js
+++ src/components/Exercise/fileItem.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faFileAlt, faSpinner, faTrash } from '@fortawesome/free-solid-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import { faFileAlt, faSpinner, faTrash } from '@fortawesome/free-solid-svg-icons'
 import './fileItem.scss'
 
 const FileItem = ({ file, deleteFile }) => {
@@ -9,21 +9,18 @@ const FileItem = ({ file, deleteFile }) => {
             <liFile
                 className="file-item"
                 key={file.name}>
-                <FontAwesomeIcon icon={faFileAlt} />
-                <p>{file.name}</p>
+                    <i class="fa fa-file-code-o fa-2x" aria-hidden="true"></i>
+                <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{file.name}</p>
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                 <div className="actions">
                     <div className="loading"></div>
-                    {file.isUploading && <FontAwesomeIcon
-                        icon={faSpinner} className="fa-spin"
-                        onClick={() => deleteFile(file.name)} />
+                    {file.isUploading && <i class="fa fa-spinner" aria-hidden="true"  onClick={() => deleteFile(file.name)}/>
                     }
                     {!file.isUploading &&
-                        <FontAwesomeIcon icon={faTrash}
-                            onClick={() => deleteFile(file.name)} />
-                    }
+                        <i class="fa fa-trash" aria-hidden="true" onClick={() => deleteFile(file.name)} />
+             }
                 </div>
             </liFile>
         </>
